a3ee315b203e15665fe33c7fdecfc8bb08e2de35
FFRAust <fraco@mail.austral.edu.ar>
2024-04-06 15:26:50 -0300

    zerotohero

--- src/rpart/141_gridsearch_esqueleto02042024.r
+++ src/rpart/141_gridsearch_esqueleto02042024.r
@@ -11,7 +11,7 @@ require("parallel")
 
 PARAM <- list()
 # reemplazar por las propias semillas
-PARAM$semillas <- c(102191, 200177, 410551, 552581, 892237)
+PARAM$semillas <- c(200041, 300043, 400051, 500083, 600091)
 
 #------------------------------------------------------------------------------
 # particionar agrega una columna llamada fold a un dataset
@@ -108,20 +108,25 @@ archivo_salida <- "./exp/HT2020/gridsearch.txt"
 # genero la data.table donde van los resultados del Grid Search
 tb_grid_search <- data.table( max_depth = integer(),
                               min_split = integer(),
-                              ganancia_promedio = numeric() )
+                              cp = numeric(),
+                              min_bucket = numeric(),
+                              ganancia_promedio = numeric() 
+                              )
 
 
 # itero por los loops anidados para cada hiperparametro
 
-for (vmax_depth in c(4, 6, 8, 10, 12, 14)) {
-  for (vmin_split in c(1000, 800, 600, 400, 200, 100, 50, 20, 10)) {
-    # notar como se agrega
+for (vcp in c(-1, -0.5, 0, 0.1)) {
+  for (vmax_depth in c(4, 5, 6, 7, 8, 9, 10)){ 
+    for (vmin_split in c(10, 30, 50, 100, 200, 400, 600)){
+      for (vmin_bucket in c(2, 10, 20, 30, 50, vmin_split/3)){
+ # notar como se agrega
 
     # vminsplit  minima cantidad de registros en un nodo para hacer el split
     param_basicos <- list(
-      "cp" = -0.5, # complejidad minima
+      "cp" = vcp, # complejidad minima
       "minsplit" = vmin_split,
-      "minbucket" = 5, # minima cantidad de registros en una hoja
+      "minbucket" = 2, # minima cantidad de registros en una hoja
       "maxdepth" = vmax_depth
     ) # profundidad máxima del arbol
 
@@ -131,8 +136,9 @@ for (vmax_depth in c(4, 6, 8, 10, 12, 14)) {
     # agrego a la tabla
     tb_grid_search <- rbindlist( 
       list( tb_grid_search, 
-            list( vmax_depth, vmin_split, ganancia_promedio) ) )
-
+            list( vcp, vmax_depth, vmin_bucket, vmin_split, ganancia_promedio) ) )
+      }
+    }
   }
 
   # escribo la tabla a disco en cada vuelta del loop mas externo
