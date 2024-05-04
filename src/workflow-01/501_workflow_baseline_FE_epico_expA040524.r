390fb4d17b79e798b8fb1a0c6e21cc31c903643c
FFRAust <fraco@mail.austral.edu.ar>
2024-05-04 17:19:32 -0300

    linea 24 carpeta 040524

--- src/workflow-01/501_workflow_baseline_FE_epico_expA040524.r
+++ src/workflow-01/501_workflow_baseline_FE_epico_expA040524.r
@@ -21,7 +21,7 @@ envg$EXPENV$repo_dir <- "~/labo2024v1/"
 envg$EXPENV$datasets_dir <- "~/buckets/b1/datasets/"
 envg$EXPENV$arch_sem <- "mis_semillas.txt"
 
-EXP_CODE = "FE_experimento_A_ratios"
+EXP_CODE = "FE_experimento_A_ratios_040524"
 
 # default
 envg$EXPENV$gcloud$RAM <- 64
